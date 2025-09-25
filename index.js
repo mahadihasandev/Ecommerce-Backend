7c9352e6fa6d122e6a6814ce5e9ecaa906cacfff
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-09-25 10:28:28 +0600

    get Data

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const Product=require('./DB.mjs')
+const app = express()
+var cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send(Product)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
