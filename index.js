2b9cb1328e0d78407953a124bd2f1c3efd2fd86a
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-09-30 13:03:47 +0600

    post route

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
-const Product=require('./DB.mjs')
 const app = express()
 var cors = require('cors')
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
+
+let users=[
+  {firstName:"mahadi",lastName:"hasan",email:"arnob4all@gmail.com",pass:"12345678"},
+  {firstName:"hasan",lastName:"arnob",email:"arnob1all@gmail.com",pass:"123456789"},
+]
 
 app.get('/', (req, res) => {
-  res.send(Product,console.log(Product)
-  )
+  res.send(users)
+  
+})
+
+app.post("/",(req,res)=>{
+  console.log(req.body);
+  
+  users.push(req.body)
 })
 
 app.listen(port, () => {
